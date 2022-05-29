73e2ea42c3e39505acc6ee24a63e60548985fcb6
YanivWein24 <yanivwein22@gmail.com>
2022-05-29 21:31:48 +0300

    All the other stuff - now its fully working

--- src/App.js
+++ src/App.js
@@ -3,37 +3,65 @@ import axios from 'axios';
 
 function App() {
 
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
   const API = `${process.env.REACT_APP_API_KEY}`
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=jerusalem&appid=${API}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API}`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+
+      })
+      setLocation('') //remove the query city name after the search
+    }
+  }
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          placeholder="Enter Location"
+          onKeyPress={searchLocation}
+          type="text" />
+      </div>
+
       <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>Jerusalem, IL</p>
+        {data.name != undefined &&
+
+          <div className="top">
+            <div className="location">
+              <p>{data.name}, {data.sys.country}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{Math.round(data.main.temp)}°C</h1> : null}
+            </div>
+            <div className="description">
+              <p>{data.weather ? data.weather[0].description : null}</p>
+            </div>
           </div>
-          <div className="temp">
-            <h1>30°C</h1>
+        }
+        {data.name != undefined &&
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">{data.main ? Math.round(data.main.feels_like) : null}°C</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className="bold">{data.main ? data.main.humidity : null}%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p className="bold">{data.wind ? data.wind.speed : null} KM/H</p>
+              <p>Winds</p>
+            </div>
           </div>
-          <div className="description">
-            <p>Cloudy</p>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">31 degrees</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold">20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">12 KM/H</p>
-            <p>Winds</p>
-          </div>
-        </div>
+        }
       </div>
     </div>
   );
